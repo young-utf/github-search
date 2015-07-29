21f8ed1fa422eebf20a8c1796296c21dabec6176
young-utf <young.utf@gmail.com>
2015-07-30 03:00:04 +0900

    Main

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -7,19 +7,31 @@ var Router = require('react-router');
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
-    mixins: [Router.State],
+    mixins: [Router.State, ReactFireMixin],
     getInitialState: function () {
         return {
-            notes: [],
-            bio: {},
-            repos: []
+            notes: [1,2,3,4,5],
+            bio: {name: 'Tyler'},
+            repos: [1,2,3]
         };
+    },
+    componentDidMount: function () {
+        //this.ref = new Firebase('https://github-note-taker.firebaseio.com');
+        //var childRef = this.ref.child(this.getParams().username);
+        //this.bindAsArray(childRef, 'notes');
+    },
+    componentWillUnmount: function () {
+        this.unbind('notes');
+    },
+    handleAddNote: function (newNote) {
+        this.ref.child(this.getParams().username).set(this.state.notes.concat)[newNote];
     },
     render: function () {
         var username = this.getParams().username;
-        console.log(username);
         return (
             <div className="row">
                 <div className="col-md-4">
@@ -29,7 +41,10 @@ var Profile = React.createClass({
                     <Repos username={username} repos={this.state.repos} />
                 </div>
                 <div className="col-md-4">
-                    <Notes username={username} notes={this.state.notes} />
+                    <Notes
+                        username={username}
+                        notes={this.state.notes}
+                        addNote={this.handleAddNote} />
                 </div>
             </div>
         )
