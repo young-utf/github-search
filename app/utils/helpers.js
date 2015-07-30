2ec13e8f14c08ea9bfd32b607824455c34de9311
young-utf <young.utf@gmail.com>
2015-07-31 03:22:56 +0900

    axios added

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,4 +1,20 @@
 /**
  * Created by youngmoon on 7/31/15.
  */
-var axios
+
+var axios = require('axios');
+
+function getRepos (username) {
+    return axios.get('https://api.github.com/users/' + username + '/repos');
+};
+
+function getUserInfo (username) {
+    return axios.get('https://api.github.com/users/' + username);
+};
+
+var promiseObj = getRepos('young-utf');
+
+promiseObj
+    .then(function (data) {
+        console.log(data);
+    });
